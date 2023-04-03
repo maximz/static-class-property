2906c0914849f4be0793797e032f9b7de48c6523
Maxim Zaslavsky <maxim@maximzaslavsky.com>
2023-04-02 18:39:32 -1000

    types

--- classproperty/__init__.py
+++ classproperty/__init__.py
@@ -3,6 +3,7 @@
 __author__ = """Maxim Zaslavsky"""
 __email__ = "maxim@maximz.com"
 __version__ = "0.0.1"
+from typing import Any
 
 
 class classproperty(property):
@@ -14,5 +15,5 @@ class classproperty(property):
     Use this when you want to mark a method as @property and @staticmethod, or as @property and @classmethod. That doesn't work out of the box; you need this decorator instead.
     """
 
-    def __get__(self, cls, owner):
+    def __get__(self, cls: None, owner: Any) -> Any:
         return classmethod(self.fget).__get__(None, owner)()
