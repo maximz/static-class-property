3024dddea8e8076f44d550d76a7e7267b1fcca87
Maxim Zaslavsky <maxim@maximzaslavsky.com>
2023-04-20 18:52:30 -0700

    Make readonly

--- static_class_property/__init__.py
+++ static_class_property/__init__.py
@@ -6,14 +6,96 @@ __version__ = "0.0.1"
 from typing import Any
 
 
-class classproperty(property):
+class GetOnlyProperty(object):
+    """Like standard property, but always read-only: doesn't allow set or delete."""
+
+    # Based on https://docs.python.org/3/howto/descriptor.html#properties
+    def __init__(self, fget=None, doc=None):
+        self.fget = fget
+        if doc is None and fget is not None:
+            doc = fget.__doc__
+        self.__doc__ = doc
+        self._name = ""
+
+    def __set_name__(self, owner, name):
+        self._name = name
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
+        if self.fget is None:
+            raise AttributeError(f"property '{self._name}' has no getter")
+        return self.fget(obj)
+
+    def getter(self, fget):
+        prop = type(self)(fget=fget, doc=self.__doc__)
+        prop._name = self._name
+        return prop
+
+    def __set__(self, obj, value):
+        # Prevents set on class *instance*. (Doesn't prevent set on the class itself - that's handled separately)
+        raise AttributeError(f"can't set attribute '{self._name}'")
+
+    def __delete__(self, obj):
+        # TODO: test whether this prevents delete on a class instance.
+        raise AttributeError(f"can't delete attribute '{self._name}'")
+
+
+class classproperty(GetOnlyProperty):
     """
-    A static readonly property on a class.
+    A static, readonly property on a class.
     From https://stackoverflow.com/a/7864317/130164
     Alternatives: https://stackoverflow.com/a/5191224/130164 and https://stackoverflow.com/a/5192374/130164
 
     Use this when you want to mark a method as @property and @staticmethod, or as @property and @classmethod. That doesn't work out of the box; you need this decorator instead.
+
+    Make sure to use the metaclass to make the property read-only on the class itself, not just on instances.
+    Without the metaclass, setting Class.property = "test" will work, but setting Class().property = "test" will not.
+    This is because Class.property just changes the binding.
     """
 
-    def __get__(self, cls: None, owner: Any) -> Any:
-        return classmethod(self.fget).__get__(None, owner)()
+    def __get__(self, instance: None, instance_type: Any) -> Any:
+        #         return self.fget.__get__(None, objtype)()
+        #         return self.fget(objtype) # this works unless also wrapped in @classmethod
+
+        # todo: can we simplify the fget in the property? test to make sure it works w/ and w/o classmethod decorator too?
+
+        # Wrap the fget in a classmethod, then call it with the class as the cls
+        wrapped_property_getter = classmethod(self.fget)
+        return wrapped_property_getter.__get__(
+            instance, instance_type
+        )()  # TODO: instance, or None?
+
+
+class MetaClassContainingClassProperties(type):
+    def __new__(meta, name, bases, attrs):
+        # TODO: Run monkeytype and get types for this.
+        # Find which attrs are classproperties
+        attrs_that_should_not_be_edited_after_definition = [
+            attrname
+            for attrname, attrval in attrs.items()
+            if isinstance(attrval, classproperty)
+        ]
+
+        new_instance = super().__new__(meta, name, bases, attrs)
+
+        # Store names of anything in attrs that isinstance classproperty
+        new_instance._cantchange = attrs_that_should_not_be_edited_after_definition
+
+        return new_instance
+
+    def __setattr__(cls, attr, value):
+        # Overload as in https://stackoverflow.com/a/42360496/130164
+        # Prevents setting the property on the class itself (doesn't prevent set on a class instance - that's handled separately)
+
+        # Skip this check when the initial set of _cantchange is performed by __new__()
+        if attr != "_cantchange":
+
+            if hasattr(cls, "_cantchange"):  # Skip this check if chaining metaclasses
+
+                if attr in cls._cantchange:
+                    # Reject setting the property on the class itself
+                    raise ValueError(f"Cannot set {attr}")
+
+        # Process the change
+        return super().__setattr__(attr, value)
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,9 +1,12 @@
-from static_class_property import classproperty
+import pytest
+from static_class_property import MetaClassContainingClassProperties, classproperty
 
 
 class ClassWithStaticProperty:
+    # normal attribute
     class_variable = "extra"
 
+    # our special static property
     @classproperty
     def static_property(cls):
         return f"static_property_{cls.class_variable}"
@@ -11,6 +14,128 @@ class ClassWithStaticProperty:
 
 def test_classproperty():
     assert ClassWithStaticProperty.static_property == "static_property_extra"
+
     # reacts to class changes
     ClassWithStaticProperty.class_variable = "new"
     assert ClassWithStaticProperty.static_property == "static_property_new"
+
+    # works in an instance
+    instance = ClassWithStaticProperty()
+    assert instance.static_property == "static_property_new"
+
+    # instance reacts to class changes (even if instance was created before the change)
+    ClassWithStaticProperty.class_variable = "new2"
+    assert ClassWithStaticProperty.static_property == "static_property_new2"
+    assert instance.class_variable == "new2"
+    assert instance.static_property == "static_property_new2"
+
+    # but no reaction to instance changes! still points to class-level value.
+    instance.class_variable = "new3"
+    assert ClassWithStaticProperty.class_variable == "new2"
+    assert instance.static_property == "static_property_new2"
+    assert ClassWithStaticProperty.static_property == "static_property_new2"
+
+    # Can't set on instance
+    with pytest.raises(AttributeError, match="can't set attribute"):
+        instance.static_property = "replaced"
+    assert instance.static_property == "static_property_new2"
+    assert ClassWithStaticProperty.static_property == "static_property_new2"
+
+    # But can set on class (and it changes for all instances), since we are not using the metaclass
+    ClassWithStaticProperty.static_property = "replaced"
+    assert ClassWithStaticProperty.static_property == "replaced"
+    assert instance.static_property == "replaced"
+
+
+####
+
+
+class ClassWithStaticPropertyAndMetaclass(metaclass=MetaClassContainingClassProperties):
+    class_variable = "extra"
+
+    @classproperty
+    def static_property(cls):
+        return f"static_property_{cls.class_variable}"
+
+
+def test_classproperty_readonly():
+    pass
+    Foo.backend  # "mybackend"
+    # Can't change before initialization
+    Foo.backend = "test"  # ValueError: cannot set backend
+
+    # Can't change after initialization
+    f = Foo()
+    f.backend  # "mybackend"
+    f.backend = "test"  # AttributeError: can't set attribute 'backend'
+
+    # other properties work as expected
+    Foo.normal  # 5
+    Foo.normal = 6
+    Foo.normal  # 6
+    f.normal  # dunno
+    Foo().normal  # 6
+
+
+####
+
+
+class BaseClass(metaclass=MetaClassContainingClassProperties):
+    class_variable = "extra"
+
+    @classproperty
+    def static_property_not_overloaded(cls):
+        return f"static_property_not_overloaded_{cls.class_variable}"
+
+    @classproperty
+    def static_property_overloaded(cls):
+        return f"static_property_overloaded_{cls.class_variable}"
+
+
+class ChildClass(BaseClass):
+    @classproperty
+    def static_property_overloaded(cls):
+        return f"static_property_overloaded2_{cls.class_variable}"
+
+
+def test_inheritance():
+    # TODO
+    # Test inheritance. does it work if not overloaded. and can you still overload
+    pass
+
+
+####
+
+from abc import ABCMeta, abstractmethod
+
+
+def combine_classes(*args):
+    # https://stackoverflow.com/a/62849795/130164
+    name = "".join(a.__name__ for a in args)
+    return type(name, args, {})
+
+
+class AbstractBaseClass(
+    metaclass=combine_classes(MetaClassContainingClassProperties, ABCMeta)
+):
+    class_variable = "extra"
+
+    @classproperty
+    @abstractmethod
+    def static_property(cls):
+        raise NotImplementedError()
+
+
+class ConcreteClass(AbstractBaseClass):
+    class_variable = "extra2"
+
+    @classproperty
+    def static_property(cls):
+        return f"static_property_overloaded_{cls.class_variable}"
+
+
+# TODO: Change all classvar to this pattern. And mention in our docs here that this is where it should be used
+def test_abstract():
+    # TODO
+    # but for abstract base class, can we keep it abstract property somehow. add that as a test too
+    pass
